2d0b4f5f89c0ca03cfc903dddd605df045d24d2e
ducknife <154030578+ducknife@users.noreply.github.com>
2024-06-27 00:34:27 +0700

    Create 5016.c

---
+++ Array2d/5016.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int n;
+    scanf("%d", &n);
+    int a[n][n], x = 1;
+    int h1 = 0, h2 = n - 1, c1 = 0, c2 = n - 1;
+    while (h1 <= h2 && c1 <= c2){
+        for (int i = c1; i <= c2; i++){
+            a[h1][i] = x;
+            ++x; 
+        }
+        h1++;
+        for (int i = h1; i <= h2; i++){
+            a[i][c2] = x;
+            ++x;
+        }
+        --c2;
+        if (h1 <= h2) 
+            for (int i = c2; i >= c1; i--){
+                a[h2][i] = x;
+                ++x;
+            }
+        --h2;
+        if (c1 <= c2)
+            for (int i = h2; i >= h1; i--){
+                a[i][c1] = x;
+                ++x;
+            }
+        c1++;
+    } 
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < n; j++)
+            printf("%d ", a[i][j]);
+    printf("\n");
+    }
+}
