551cb6d769060223065f304a74a5c7d21fa823f8
ducknife <154030578+ducknife@users.noreply.github.com>
2024-06-27 00:00:16 +0700

    Create 2021.c

---
+++ 2021.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(){
+	int n;
+	int jump;
+	scanf("%d", &n);
+	for (int i = 1; i <= n; i++){
+		int k = i;
+		jump = n - 1;
+		for (int j = 1; j <= i; j++){
+			printf("%d ", k);
+			if ( jump >= 1)
+			{
+				k += jump;
+				jump --;
+			}
+		}
+		printf("\n");
+	}
+}
