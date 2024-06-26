a577a8f28972074c072b564b0cae90d0bf99e3a1
ducknife <154030578+ducknife@users.noreply.github.com>
2024-06-26 23:40:47 +0700

    Create 2024.c

---
+++ 2024.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main(){
+	int n, m;
+	scanf("%d%d", &n, &m);
+	if (n <= m){
+		for (int i = 1; i <= n; i++){
+		    for (int j = i; j <= m; j++) printf("%c", j + 64);
+		    for (int j = i - 1; j >= 1; j--) printf("%c", j + 64);
+		    printf("\n");
+		}
+	}
+	else{
+		for (int i = 1; i <= m; i++){
+			for (int j = i; j <= m; j++) printf("%c", j + 64);
+			for (int j = i - 1; j >= 1; j--) printf("%c", j + 64);
+			printf("\n");
+		}
+		for (int i = m + 1; i <= n; i++){
+			printf("%c", m + 64);
+			for ( int j = m - 1; j >= 1; j--) printf("%c", j + 64);
+			printf("\n");
+		}	
+	}
+}
