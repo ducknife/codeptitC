2c7d366f9c68d9dc3dc5c139261f90f92469088d
ducknife <154030578+ducknife@users.noreply.github.com>
2024-06-27 00:05:39 +0700

    Create 2030.c

---
+++ 2030.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+	int n;
+	scanf("%d", &n);
+	for (int i = 1; i <= n; i++){
+	    for ( int j = 1; j <= 2*i-1; j+=2) printf("%c", j + 63);
+		for ( int j = 2*i-3; j >= 1; j-=2) printf("%c", j + 63);
+		printf("\n");
+	}
+}
