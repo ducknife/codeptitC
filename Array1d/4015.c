1df11abe0efb2f660c30a2068580420d7e85786f
DasKow <154030578+ducknife@users.noreply.github.com>
2024-06-27 18:27:58 +0700

    Create 4015.c

---
+++ Array1d/4015.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int dem[100001] = {0};
+int main(){
+	int t;
+	scanf("%d", &t);
+	for (int k = 1; k <= t; k++){
+	    int n;
+	    scanf("%d", &n);
+	    int a[n];
+	    for (int i = 0; i < n; i++){
+		    scanf("%d", &a[i]);
+		    dem[a[i]]++;
+	    }
+	    printf("Test %d:\n", k);
+	    for (int i = 0; i < n; i++){
+			if (dem[a[i]] >= 1){
+			    printf("%d xuat hien %d lan\n", a[i], dem[a[i]]);
+			    dem[a[i]] = 0;
+            }
+	    }
+	}
+}
